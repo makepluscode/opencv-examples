a1c9fa62b8a42c6d2809ff0010cce8b9e7568bab
makepluscode <makepluscode@gmail.com>
2021-10-03 05:13:05 -0700

    [001] Update initial code

---
+++ 001-hello-opencv/src/main.cpp
@@ -0,0 +1,26 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	cout << "opencv version : " << CV_VERSION << endl;
+    
+    Mat img;
+    img = imread("./img/google.png");
+    
+    if(!img.empty())
+    {
+        namedWindow("image");
+        imshow("image", img);
+        waitKey(0);
+    }
+    else
+    {
+    	cout << "imgae not found " << CV_VERSION << endl;
+    }
+
+    return 0;
+}
